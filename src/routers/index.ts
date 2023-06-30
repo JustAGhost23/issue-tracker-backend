dd692e4c8a1a33dd17646df953fcd6d44e2fd6c9
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-06-30 23:04:00 +0530

    feat: mount routers to appRouter

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import authRouter from "./authRouter";
+import userRouter from "./userRouter";
+import projectRouter from "./projectRouter";
+import ticketRouter from "./ticketRouter";
 
 const appRouter: Router = Router();
+
+appRouter.use("/auth", authRouter);
+appRouter.use("/user", userRouter);
+appRouter.use("/project", projectRouter);
+appRouter.use("/ticket", ticketRouter);
 
 export default appRouter;
