d3cb02b130a586c11cd78a8f2496481a8dfb457d
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-07-01 01:12:11 +0530

    feat: connect passport middleware to express app

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { json, urlencoded, Express } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { corsOptions } from "./config/cors.js";
+import passport from "./middlewares/passportAuth.js";
 import appRouter from "./routers/index.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -12,6 +13,8 @@ app.use(cookieParser());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cors(corsOptions));
+
+app.use(passport.initialize())
 
 app.use("/api", appRouter);
 
