02aa43329f99b69708f722ee53172fbc9896b31b
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-07-01 01:08:56 +0530

    feat: add passport middleware to handle Google authentication

--- src/middlewares/passportAuth.ts
+++ src/middlewares/passportAuth.ts
@@ -1,8 +1,11 @@
 import passport from "passport";
 import { Strategy as JwtStrategy } from "passport-jwt";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { prisma } from "../config/db.js";
+import { Provider } from "@prisma/client";
 import { cookieExtractor } from "../utils/cookieExtractor.js";
 import { JwtPayload } from "jsonwebtoken";
+import crypto from "crypto";
 
 const JwtAuthCallback = async (jwt_payload: JwtPayload, done: any) => {
   try {
@@ -27,6 +30,84 @@ passport.use(
       secretOrKey: process.env.TOKEN_SECRET,
     },
     JwtAuthCallback
+  )
+);
+
+const GoogleAuthCallback = async (
+  accessToken: any,
+  refreshToken: any,
+  profile: any,
+  done: any
+) => {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        googleId: profile.id,
+        provider: {
+          has: Provider.GOOGLE,
+        },
+      },
+    });
+    if (user) {
+      return done(null, user);
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: profile._json.email,
+      },
+    });
+    if (existingUser) {
+      existingUser.provider = [Provider.GOOGLE, Provider.LOCAL];
+      existingUser.googleId = profile.id;
+
+      const savedUser = await prisma.user.update({
+        where: {
+          email: profile._json.email,
+        },
+        data: existingUser,
+      });
+      return done(null, savedUser);
+    }
+
+    const username = profile.username;
+
+    const foundUsername = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+    if (foundUsername) {
+      username.concat("#");
+      username.concat(crypto.randomInt(0, 10000));
+    }
+
+    console.log("User does not exist");
+    const newUser = await prisma.user.create({
+      data: {
+        email: profile._json.email,
+        username: username,
+        name: profile.displayName,
+        provider: [Provider.GOOGLE],
+        googleId: profile.id,
+      },
+    });
+    return done(null, newUser);
+  } catch (err) {
+    return done(err, false);
+  }
+};
+
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: "/api/auth/google/callback",
+      proxy: true,
+    },
+    GoogleAuthCallback
   )
 );
 
