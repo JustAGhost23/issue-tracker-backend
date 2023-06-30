c847a8a3165e4067ae2f8a3033de6344a043e9ab
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-06-30 21:07:42 +0530

    feat: setup redis client in `db.ts`

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+import { createClient } from "redis";
 /**
  * This is the code that connects to the database.
  */
+
+const redisClient = createClient();
+
+redisClient.on("error", function (err) {
+  console.log(
+    "Main Client: Could not establish a connection with redis. " + err
+  );
+});
+redisClient.on("connect", function (err) {
+  console.log("Main Client: Connected to redis successfully");
+});
+redisClient.connect();
 
 const prisma: PrismaClient = new PrismaClient({
   log: ["query", "warn", "error"],
 });
 
-export { prisma };
+export { prisma, redisClient };
