1fba7baaadf10a94033169247b5152433db5dedd
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-06-30 21:04:16 +0530

    fix: add port to .env

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,6 @@ app.use(cookieParser());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
-app.listen(3000, () => {
-  console.log(`Listening on port 3000`);
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
