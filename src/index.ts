b897584aa2fa42328aafcc9c64de95da62bcb0d2
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-06-30 22:39:49 +0530

    feat: mount appRouter to app via `/api` route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { json, urlencoded, Express } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { corsOptions } from "./config/cors.js";
+import appRouter from "./routers/index.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -11,6 +12,8 @@ app.use(cookieParser());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cors(corsOptions));
+
+app.use("/api", appRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
