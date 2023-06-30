c86da18ca8868965203d6a7f299f9cdafa8f7c60
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-06-30 21:11:22 +0530

    feat: connect cors options to app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { json, urlencoded, Express } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import { corsOptions } from "./config/cors.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,6 +10,7 @@ const app: Express = express();
 app.use(cookieParser());
 app.use(json());
 app.use(urlencoded({ extended: false }));
+app.use(cors(corsOptions));
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
