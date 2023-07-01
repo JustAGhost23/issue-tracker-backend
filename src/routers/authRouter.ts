60086fc59b9de62d32f57183fbda7b39ae80934c
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-07-01 09:51:24 +0530

    feat: mount register function to `/api/auth/register`

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { registerValidator, register } from "../controllers/auth/register";
 
 const authRouter: Router = Router();
+
+/**
+ @route /api/auth/register
+ @desc Register using username/password
+ */
+authRouter.post('/register', registerValidator, register);
 
 export default authRouter;
