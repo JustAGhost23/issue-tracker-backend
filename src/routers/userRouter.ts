697006fda91ec67a4bce41d9829860b2d5906c97
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-07-01 11:24:37 +0530

    feat: mount get all users function to `/api/user`

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import passport from "passport";
+import { getAllUsersValidator, getAllUsers } from "../controllers/user/getAllUsers";
+import passport from "../middlewares/passportAuth";
 
 const passportJWT = passport.authenticate("jwt", { session: false });
 
 const userRouter: Router = Router();
+
+/**
+ @route /api/user/
+ @desc Get all users
+ */
+userRouter.get('/', passportJWT, getAllUsersValidator, getAllUsers);
 
 export default userRouter;
