ffde07bf8acda238fc19ec7faae869dd2b48f072
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-07-01 11:22:51 +0530

    feat: add passport authentication to userRouter

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import passport from "passport";
+
+const passportJWT = passport.authenticate("jwt", { session: false });
 
 const userRouter: Router = Router();
 
