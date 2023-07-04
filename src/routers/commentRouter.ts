f4324dcb0c1468e4377953f6554f91f82fa215d5
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-07-04 22:57:35 +0530

    feat: mount create new comment to `/api/comment`

--- src/routers/commentRouter.ts
+++ src/routers/commentRouter.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import passport from "../middlewares/passportAuth.js";
+import {
+  createComment,
+  createCommentValidator,
+} from "../controllers/comment/createComment.js";
 
 const passportJWT = passport.authenticate("jwt", { session: false });
 
 const commentRouter: Router = Router();
+
+/**
+ @route /api/comment/
+ @desc Create new comment
+ */
+commentRouter.post("/", passportJWT, createCommentValidator, createComment);
 
 export default commentRouter;
