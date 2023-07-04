391f49d7aa9ac5060dfc72b9f26bb84adc0e72d6
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-07-04 23:12:04 +0530

    feat: mount delete comment to `/api/comment/:commentId/delete`

--- src/routers/commentRouter.ts
+++ src/routers/commentRouter.ts
@@ -4,6 +4,10 @@ import {
   createComment,
   createCommentValidator,
 } from "../controllers/comment/createComment.js";
+import {
+  deleteComment,
+  deleteCommentValidator,
+} from "../controllers/comment/deleteComment.js";
 
 const passportJWT = passport.authenticate("jwt", { session: false });
 
@@ -14,5 +18,16 @@ const commentRouter: Router = Router();
  @desc Create new comment
  */
 commentRouter.post("/", passportJWT, createCommentValidator, createComment);
+
+/**
+ @route /api/comment/:commentId/delete
+ @desc Delete comment
+ */
+commentRouter.post(
+  "/:commentId/delete",
+  passportJWT,
+  deleteCommentValidator,
+  deleteComment
+);
 
 export default commentRouter;
