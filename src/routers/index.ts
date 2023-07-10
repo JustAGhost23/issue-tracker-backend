f15c6b583a101d93352c67fe8213d85b2aa25f08
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-07-10 21:29:38 +0530

    feat: mount role router to `/api/role`

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
 import userRouter from "./userRouter.js";
+import roleRouter from "./roleRouter.js";
 import projectRouter from "./projectRouter.js";
 import ticketRouter from "./ticketRouter.js";
 import commentRouter from "./commentRouter.js";
@@ -9,6 +10,7 @@ const appRouter: Router = Router();
 
 appRouter.use("/auth", authRouter);
 appRouter.use("/user", userRouter);
+appRouter.use("/role", roleRouter);
 appRouter.use("/project", projectRouter);
 appRouter.use("/ticket", ticketRouter);
 appRouter.use("/comment", commentRouter);
