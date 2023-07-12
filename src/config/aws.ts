a8ef9e586bbc1557a833fbdad3bcccb89a51ddc9
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-07-12 20:23:51 +0530

    feat: setup aws and multer config and create upload function

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -1,7 +1,29 @@
-import aws from "aws-sdk";
+import { S3Client } from "@aws-sdk/client-s3";
+import multer from "multer";
+import multerS3 from "multer-s3";
+import { prisma } from "./db.js";
+
+const s3Config = new S3Client({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY!,
+    secretAccessKey: process.env.ACCESS_SECRET!,
+  },
+});
 
 const S3_BUCKET = process.env.S3_BUCKET_NAME;
 
-aws.config.region = "eu-central-1";
+const upload = multer({
+  storage: multerS3({
+    s3: s3Config,
+    bucket: S3_BUCKET!,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
 
-export { aws, S3_BUCKET };
+export { upload };
---
+++ src/controllers/ticket/uploadFile.ts
@@ -0,0 +1,79 @@
+import { Request, RequestHandler, Response } from "express";
+import { prisma } from "../../config/db.js";
+import { validate } from "../../utils/zodValidateRequest.js";
+import { z } from "zod";
+
+// Zod schema to validate request
+const uploadFileSchema = z.object({
+  params: z.object({
+    ticketId: z.coerce
+      .number({
+        invalid_type_error: "ticketId not a number",
+        required_error: "ticketId is a required path parameter",
+      })
+      .positive({
+        message: "invalid ticketId",
+      })
+      .int({
+        message: "invalid ticketId",
+      }),
+  }),
+});
+
+/**
+ @route POST /api/ticket/:ticketId/upload
+ @type RequestHandler
+ */
+
+export const uploadFileValidator: RequestHandler = validate(uploadFileSchema);
+
+export const uploadFile = async (req: Request, res: Response) => {
+  try {
+    const file = req.file;
+    if (!file) {
+      return res.status(404).send({ error: "File not found" });
+    }
+
+    // Check if ticket exists
+    const ticket = await prisma.ticket.findUnique({
+      where: {
+        id: parseInt(req.params.ticketId),
+      },
+    });
+    if (!ticket) {
+      return res.status(404).send({ error: "Ticket not found" });
+    }
+
+    const uploadFile = await prisma.files.create({
+      data: {
+        originalName: file.originalname,
+        filename: file.filename,
+        filepath: file.path,
+        mimetype: file.mimetype,
+        size: file.size,
+        ticket: {
+          connect: {
+            id: parseInt(req.params.ticketId),
+          },
+        },
+      },
+    });
+    if (!uploadFile) {
+      res.status(500).send({
+        error: "Something went wrong while inserting file data into database",
+      });
+    }
+
+    return res.status(200).send({
+      data: {
+        uploadFile,
+      },
+      message: "Uploaded file successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      error: "Something went wrong while inserting file data into database",
+    });
+  }
+};
